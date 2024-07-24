d039f78b5256f9e3b3d2f9c2e8f39e2ff350d65d
solidity12 <volibear1001@gmail.com>
2024-07-24 17:08:44 +0900

    feat: Add deploy script code

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    weth = await ethers.deployContract("WETH", [], {});
+    await weth.waitForDeployment();
+
+    console.log("WETH contract deployed to:", await weth.getAddress());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
